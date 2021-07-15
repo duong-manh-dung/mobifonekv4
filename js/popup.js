6e3e902a608d552cc0f22a1880f02758c20a1d6c
duong-manh-dung <duongdung23@icloud.com>
2021-07-15 11:34:41 +0700

    re-write popup.js

--- js/popup.js
+++ js/popup.js
@@ -1,44 +1,317 @@
-$('.btn-signup').click(function(event) {
+//click nút đăng kí ở gói trả trước
+$('.btn-hot').click(function() {
   $('.phone').addClass('is-active');
-  var packCurrent = $('.is-light').attr('id');
-  $('#' + packCurrent).addClass('sub-active');
-  $('.' + packCurrent).show();
+  $('#hot').addClass('sub-active');
+  $('.hot').show();
+
+  // Nhấn nút huỷ tại màn nhập số điện thoại
+  $('.cancel').click(function() {
+    $('.phone').removeClass('is-active');
+    // $('.otp').removeClass('is-active');
+    // $('.noti').removeClass('is-active');
+    $('#hot').addClass('sub-active');
+    $('.hot').show();
+  });
+
+  // Nhấn nút xác nhận tại màn nhập số điện thoại
+  $('.confirm').click(function() {
+    $('.phone').removeClass('is-active');
+    $('.otp').addClass('is-active');
+    $('#hot').addClass('sub-active');
+    $('.hot').show();
+
+    // Nhấn nút huỷ tại màn nhập OTP
+    $('.cancel').click(function() {
+      $('.otp').removeClass('is-active');
+      // $('.otp').removeClass('is-active');
+      // $('.noti').removeClass('is-active');
+      $('#hot').addClass('sub-active');
+      $('.hot').show();
+    });
+
+    // Nhấn nút gửi tại màn nhập số điện thoại
+    $('.send').click(function() {
+      // $('.phone').removeClass('is-active');
+      $('.otp').removeClass('is-active');
+      $('.noti').addClass('is-active');
+      $('#hot').addClass('sub-active');
+      $('.hot').show();
+
+      // Nhấn nút đóng tại màn nhập OTP
+      $('.cancel').click(function() {
+        $('.noti').removeClass('is-active');
+        // $('.otp').removeClass('is-active');
+        // $('.noti').removeClass('is-active');
+        $('#hot').addClass('sub-active');
+        $('.hot').show();
+      });
+    });
+  });
 });
 
-$('.modal-background').click(function(event) {
+//click nút đăng kí ở gói trả trước
+$('.btn-prepaid').click(function() {
+  $('.phone').addClass('is-active');
+  $('#prepaid').addClass('sub-active');
+  $('.prepaid').show();
+
+  // Nhấn nút huỷ tại màn nhập số điện thoại
+  $('.cancel').click(function() {
+    $('.phone').removeClass('is-active');
+    // $('.otp').removeClass('is-active');
+    // $('.noti').removeClass('is-active');
+    $('#prepaid').addClass('sub-active');
+    $('.prepaid').show();
+  });
+
+  // Nhấn nút xác nhận tại màn nhập số điện thoại
+  $('.confirm').click(function() {
+    $('.phone').removeClass('is-active');
+    $('.otp').addClass('is-active');
+    $('#prepaid').addClass('sub-active');
+    $('.prepaid').show();
+
+    // Nhấn nút huỷ tại màn nhập OTP
+    $('.cancel').click(function() {
+      $('.otp').removeClass('is-active');
+      // $('.otp').removeClass('is-active');
+      // $('.noti').removeClass('is-active');
+      $('#prepaid').addClass('sub-active');
+      $('.prepaid').show();
+    });
+
+    // Nhấn nút gửi tại màn nhập số điện thoại
+    $('.send').click(function() {
+      // $('.phone').removeClass('is-active');
+      $('.otp').removeClass('is-active');
+      $('.noti').addClass('is-active');
+      $('#prepaid').addClass('sub-active');
+      $('.prepaid').show();
+
+      // Nhấn nút đóng tại màn nhập OTP
+      $('.cancel').click(function() {
+        $('.noti').removeClass('is-active');
+        // $('.otp').removeClass('is-active');
+        // $('.noti').removeClass('is-active');
+        $('#prepaid').addClass('sub-active');
+        $('.prepaid').show();
+      });
+    });
+  });
+});
+
+//click nút đăng kí ở gói trả sau
+$('.btn-postpaid').click(function() {
+  $('.phone').addClass('is-active');
+  $('#postpaid').addClass('sub-active');
+  $('.postpaid').show();
+
+  // Nhấn nút huỷ tại màn nhập số điện thoại
+  $('.cancel').click(function() {
+    $('.phone').removeClass('is-active');
+    // $('.otp').removeClass('is-active');
+    // $('.noti').removeClass('is-active');
+    $('#postpaid').addClass('sub-active');
+    $('.postpaid').show();
+  });
+
+  // Nhấn nút xác nhận tại màn nhập số điện thoại
+  $('.confirm').click(function() {
+    $('.phone').removeClass('is-active');
+    $('.otp').addClass('is-active');
+    $('#postpaid').addClass('sub-active');
+    $('.postpaid').show();
+
+    // Nhấn nút huỷ tại màn nhập OTP
+    $('.cancel').click(function() {
+      $('.otp').removeClass('is-active');
+      // $('.otp').removeClass('is-active');
+      // $('.noti').removeClass('is-active');
+      $('#postpaid').addClass('sub-active');
+      $('.postpaid').show();
+    });
+
+    // Nhấn nút gửi tại màn nhập số điện thoại
+    $('.send').click(function() {
+      // $('.phone').removeClass('is-active');
+      $('.otp').removeClass('is-active');
+      $('.noti').addClass('is-active');
+      $('#postpaid').addClass('sub-active');
+      $('.postpaid').show();
+
+      // Nhấn nút đóng tại màn nhập OTP
+      $('.cancel').click(function() {
+        $('.noti').removeClass('is-active');
+        // $('.otp').removeClass('is-active');
+        // $('.noti').removeClass('is-active');
+        $('#postpaid').addClass('sub-active');
+        $('.postpaid').show();
+      });
+    });
+  });
+});
+
+//click nút đăng kí ở gói data
+$('.btn-data').click(function() {
+  $('.phone').addClass('is-active');
+  $('#data').addClass('sub-active');
+  $('.data').show();
+
+  // Nhấn nút huỷ tại màn nhập số điện thoại
+  $('.cancel').click(function() {
+    $('.phone').removeClass('is-active');
+    // $('.otp').removeClass('is-active');
+    // $('.noti').removeClass('is-active');
+    $('#data').addClass('sub-active');
+    $('.data').show();
+  });
+
+  // Nhấn nút xác nhận tại màn nhập số điện thoại
+  $('.confirm').click(function() {
+    $('.phone').removeClass('is-active');
+    $('.otp').addClass('is-active');
+    $('#data').addClass('sub-active');
+    $('.data').show();
+
+    // Nhấn nút huỷ tại màn nhập OTP
+    $('.cancel').click(function() {
+      $('.otp').removeClass('is-active');
+      // $('.otp').removeClass('is-active');
+      // $('.noti').removeClass('is-active');
+      $('#data').addClass('sub-active');
+      $('.data').show();
+    });
+
+    // Nhấn nút gửi tại màn nhập số điện thoại
+    $('.send').click(function() {
+      // $('.phone').removeClass('is-active');
+      $('.otp').removeClass('is-active');
+      $('.noti').addClass('is-active');
+      $('#data').addClass('sub-active');
+      $('.data').show();
+
+      // Nhấn nút đóng tại màn nhập OTP
+      $('.cancel').click(function() {
+        $('.noti').removeClass('is-active');
+        // $('.otp').removeClass('is-active');
+        // $('.noti').removeClass('is-active');
+        $('#data').addClass('sub-active');
+        $('.data').show();
+      });
+    });
+  });
+});
+
+//click nút đăng kí ở gói cam kết
+$('.btn-vas').click(function() {
+  $('.phone').addClass('is-active');
+  $('#vas').addClass('sub-active');
+  $('.vas').show();
+
+  // Nhấn nút huỷ tại màn nhập số điện thoại
+  $('.cancel').click(function() {
+    $('.phone').removeClass('is-active');
+    // $('.otp').removeClass('is-active');
+    // $('.noti').removeClass('is-active');
+    $('#vas').addClass('sub-active');
+    $('.vas').show();
+  });
+
+  // Nhấn nút xác nhận tại màn nhập số điện thoại
+  $('.confirm').click(function() {
+    $('.phone').removeClass('is-active');
+    $('.otp').addClass('is-active');
+    $('#vas').addClass('sub-active');
+    $('.vas').show();
+
+    // Nhấn nút huỷ tại màn nhập OTP
+    $('.cancel').click(function() {
+      $('.otp').removeClass('is-active');
+      // $('.otp').removeClass('is-active');
+      // $('.noti').removeClass('is-active');
+      $('#vas').addClass('sub-active');
+      $('.vas').show();
+    });
+
+    // Nhấn nút gửi tại màn nhập số điện thoại
+    $('.send').click(function() {
+      // $('.phone').removeClass('is-active');
+      $('.otp').removeClass('is-active');
+      $('.noti').addClass('is-active');
+      $('#vas').addClass('sub-active');
+      $('.vas').show();
+
+      // Nhấn nút đóng tại màn nhập OTP
+      $('.cancel').click(function() {
+        $('.noti').removeClass('is-active');
+        // $('.otp').removeClass('is-active');
+        // $('.noti').removeClass('is-active');
+        $('#vas').addClass('sub-active');
+        $('.vas').show();
+      });
+    });
+  });
+});
+
+//click nút đăng kí ở gói subsidy
+$('.btn-subsidy').click(function() {
+  $('.phone').addClass('is-active');
+  $('#subsidy').addClass('sub-active');
+  $('.subsidy').show();
+
+  // Nhấn nút huỷ tại màn nhập số điện thoại
+  $('.cancel').click(function() {
+    $('.phone').removeClass('is-active');
+    // $('.otp').removeClass('is-active');
+    // $('.noti').removeClass('is-active');
+    $('#subsidy').addClass('sub-active');
+    $('.subsidy').show();
+  });
+
+  // Nhấn nút xác nhận tại màn nhập số điện thoại
+  $('.confirm').click(function() {
+    $('.phone').removeClass('is-active');
+    $('.otp').addClass('is-active');
+    $('#subsidy').addClass('sub-active');
+    $('.subsidy').show();
+
+    // Nhấn nút huỷ tại màn nhập OTP
+    $('.cancel').click(function() {
+      $('.otp').removeClass('is-active');
+      // $('.otp').removeClass('is-active');
+      // $('.noti').removeClass('is-active');
+      $('#subsidy').addClass('sub-active');
+      $('.subsidy').show();
+    });
+
+    // Nhấn nút gửi tại màn nhập số điện thoại
+    $('.send').click(function() {
+      // $('.phone').removeClass('is-active');
+      $('.otp').removeClass('is-active');
+      $('.noti').addClass('is-active');
+      $('#subsidy').addClass('sub-active');
+      $('.subsidy').show();
+
+      // Nhấn nút đóng tại màn nhập OTP
+      $('.cancel').click(function() {
+        $('.noti').removeClass('is-active');
+        // $('.otp').removeClass('is-active');
+        // $('.noti').removeClass('is-active');
+        $('#subsidy').addClass('sub-active');
+        $('.subsidy').show();
+      });
+    });
+  });
+});
+
+$('.modal-background').click(function() {
   $('.phone').removeClass('is-active');
   $('.otp').removeClass('is-active');
   $('.noti').removeClass('is-active');
 });
 
-$('.delete').click(function(event) {
+$('.delete').click(function() {
   $('.phone').removeClass('is-active');
   $('.otp').removeClass('is-active');
   $('.noti').removeClass('is-active');
-});
-
-$('.cancel').click(function(event) {
-  $('.phone').removeClass('is-active');
-  $('.otp').removeClass('is-active');
-  $('.noti').removeClass('is-active');
-  var packCurrent = $('.is-light').attr('id');
-  $('#' + packCurrent).addClass('sub-active');
-  $('.' + packCurrent).show();
-});
-
-$('.confirm').click(function(event) {
-  $('.phone').removeClass('is-active');
-  $('.otp').addClass('is-active');
-  var packCurrent = $('.is-light').attr('id');
-  $('#' + packCurrent).addClass('sub-active');
-  $('.' + packCurrent).show();
-});
-
-$('.send').click(function(event) {
-  $('.phone').removeClass('is-active');
-  $('.otp').removeClass('is-active');
-  $('.noti').addClass('is-active');
-  var packCurrent = $('.is-light').attr('id');
-  $('#' + packCurrent).addClass('sub-active');
-  $('.' + packCurrent).show();
 });
